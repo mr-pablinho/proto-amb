537b6638bd88d98b9d17c39fcc22173de03aee48
Pablo Merchan-Rivera <pmr.prod@outlook.com>
2026-01-29 21:33:47 -0500

    feat: Initialize ChromaDB and update `MODEL_NAME` to `gemini-2.5-flash` in `config.py`.

--- config.py
+++ config.py
@@ -6,10 +6,13 @@ load_dotenv()
 
 # Configuración centralizada
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-MODEL_NAME = "gemini-1.5-pro" # Usamos Pro por la ventana de contexto amplia
+# MODEL_NAME = "gemini-1.5-pro" # Usamos Pro por la ventana de contexto amplia
+MODEL_NAME = "gemini-2.5-flash" 
+
 # EMBEDDING_MODEL = "models/embedding-001"
 EMBEDDING_MODEL = "all-MiniLM-L6-v2"  # Modelo ligero, rápido y potente
 
 # Rutas
 LEGAL_DOCS_PATH = "leyes"
 VECTOR_DB_PATH = "chroma_db"
+
